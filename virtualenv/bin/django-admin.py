62140c5d5e56574d372440f50870d03c6648b00d
Daniel Lachmann-Nishibane <dalachmann@gmail.com>
2017-12-27 02:56:44 +0100

    cleaning

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/danisuke0781/OneDrive/WebDeveloper_Training/book-example/virtualenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
